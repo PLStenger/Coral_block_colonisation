173cac516b94d5347824b72adb24ac9a64dddd44
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2023-04-30 18:55:06 +0700

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -5,14 +5,14 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-# WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data/Original_reads_16S
-# OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check/Original_reads_16S
+# WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_16S
+# OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_16S
 
-# WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data/Original_reads_18S
-# OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check/Original_reads_18S
+# WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_18S
+# OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_18S
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data/Original_reads_TUFA
-OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check/Original_reads_TUFA
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_TUFA
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_TUFA
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -35,8 +35,8 @@ conda activate multiqc
 
 multiqc $OUTPUT
 
-## WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data/Original_reads_ITS
-## OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check/Original_reads_ITS
+## WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_ITS
+## OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_ITS
 ## 
 ## # Make the directory (mkdir) only if not existe already(-p)
 ## mkdir -p $OUTPUT
