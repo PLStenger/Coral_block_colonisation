714236322bf73b53742f998c821c77a72d303289
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2023-05-16 16:55:34 +0700

    Update 99_junk.sh

--- 00_scripts/99_junk.sh
+++ 00_scripts/99_junk.sh
@@ -1,35 +1,105 @@
 #!/usr/bin/env bash
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/Rhabdastrella_globostellata_microbiome/05_QIIME2
-OUTPUT=/scratch_vol1/fungi/Rhabdastrella_globostellata_microbiome/05_QIIME2/visual
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/05_QIIME2/Original_reads_TUFA
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/05_QIIME2/Original_reads_TUFA/visual
 
-DATABASE=/scratch_vol1/fungi/Rhabdastrella_globostellata_microbiome/98_database_files
+DATABASE=/scratch_vol1/fungi/Coral_block_colonisation/98_database_files
+TMPDIR=/scratch_vol1
+
+# Aim: perform diversity metrics and rarefaction
+
+# https://chmi-sops.github.io/mydoc_qiime2.html#step-8-calculate-and-explore-diversity-metrics
+# https://docs.qiime2.org/2018.2/tutorials/moving-pictures/#alpha-rarefaction-plotting
+# https://forum.qiime2.org/t/how-to-decide-p-sampling-depth-value/3296/6
+
+# Use QIIME2’s diversity core-metrics-phylogenetic function to calculate a whole bunch of diversity metrics all at once. 
+# Note that you should input a sample-depth value based on the alpha-rarefaction analysis that you ran before.
+
+# sample-depth value choice : 
+# We are ideally looking for a sequencing depth at the point where these rarefaction curves begin to level off (indicating that most of the relevant diversity has been captured).
+# This helps inform tough decisions that we need to make when some samples have lower sequence counts and we need to balance the priorities that you want to choose 
+# a value high enough that you capture the diversity present in samples with high counts, but low enough that you don’t get rid of a ton of your samples.
 
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2021.4
 
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p pcoa
+mkdir -p export/pcoa
 
-qiime taxa barplot \
-  --i-table core/Table.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_RepSeq.qza \
-  --m-metadata-file $DATABASE/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv
+# I'm doing this step in order to deal the no space left in cluster :
+export TMPDIR='/scratch_vol1/fungi'
+echo $TMPDIR
 
-qiime taxa barplot \
-  --i-table core/ConTable.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza \
-  --m-metadata-file $DATABASE/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv
-  
-qiime taxa barplot \
+# core_metrics_phylogenetic:
+############################
+    # Aim: Applies a collection of diversity metrics to a feature table
+    # Use: qiime diversity core-metrics-phylogenetic [OPTIONS]
+    
+    # With 4202 -> 0 samples deleted
+
+qiime diversity core-metrics-phylogenetic \
+       --i-phylogeny tree/rooted-tree.qza \
+       --i-table core/Table.qza \
+       --p-sampling-depth 15893 \
+       --m-metadata-file $DATABASE/sample-metadata.tsv \
+       --o-rarefied-table core/RarTable.qza \
+       --o-observed-features-vector core/Vector-observed_asv.qza \
+       --o-shannon-vector core/Vector-shannon.qza \
+       --o-evenness-vector core/Vector-evenness.qza \
+       --o-faith-pd-vector core/Vector-faith_pd.qza \
+       --o-jaccard-distance-matrix core/Matrix-jaccard.qza \
+       --o-bray-curtis-distance-matrix core/Matrix-braycurtis.qza \
+       --o-unweighted-unifrac-distance-matrix core/Matrix-unweighted_unifrac.qza \
+       --o-weighted-unifrac-distance-matrix core/Matrix-weighted_unifrac.qza \
+       --o-jaccard-pcoa-results pcoa/PCoA-jaccard.qza \
+       --o-bray-curtis-pcoa-results pcoa/PCoA-braycurtis.qza \
+       --o-unweighted-unifrac-pcoa-results pcoa/PCoA-unweighted_unifrac.qza \
+       --o-weighted-unifrac-pcoa-results pcoa/PCoA-weighted_unifrac.qza \
+       --o-jaccard-emperor visual/Emperor-jaccard.qzv \
+       --o-bray-curtis-emperor visual/Emperor-braycurtis.qzv \
+       --o-unweighted-unifrac-emperor visual/Emperor-unweighted_unifrac.qzv \
+       --o-weighted-unifrac-emperor visual/Emperor-weighted_unifrac.qzv
+       
+       
+
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/05_QIIME2/Original_reads_TUFA
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/05_QIIME2/Original_reads_TUFA/visual
+
+DATABASE=/scratch_vol1/fungi/Coral_block_colonisation/98_database_files
+TMPDIR=/scratch_vol1
+
+
+
+
+cd $WORKING_DIRECTORY
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2021.4
+
+# I'm doing this step in order to deal the no space left in cluster :
+export TMPDIR='/scratch_vol1/fungi'
+echo $TMPDIR
+
+threads=FALSE
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p taxonomy
+mkdir -p export/taxonomy
+
+# I'm doing this step in order to deal the no space left in cluster :
+export TMPDIR='/scratch_vol1/fungi'
+echo $TMPDIR
+
+
+
+ qiime taxa barplot \
   --i-table core/RarTable.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza \
+  --i-taxonomy taxonomy/taxonomy_reads-per-batch_RarRepSeq_vsearch.qza \
   --m-metadata-file $DATABASE/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv  
+  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq_vsearch.qzv 
   
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RepSeq
-
+  
+  qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq_vsearch.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq_vsearch
