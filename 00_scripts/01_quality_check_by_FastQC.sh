c6162107d85b2a2ac9689e38c0017944947aeae2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2023-08-04 13:26:19 +0800

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -5,14 +5,33 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-# WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_16S
-# OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_16S
 
-# WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_18S
-# OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_18S
+## WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_TUFA/Reads
+## OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_TUFA
+## 
+## # Make the directory (mkdir) only if not existe already(-p)
+## mkdir -p $OUTPUT
+## 
+## eval "$(conda shell.bash hook)"
+## conda activate fastqc
+## 
+## cd $WORKING_DIRECTORY
+## 
+## for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+## do
+##       fastqc $FILE -o $OUTPUT
+## done ;
+## 
+## 
+## conda deactivate fastqc
+## conda activate multiqc
+## 
+## # Run multiqc for quality summary
+## 
+## multiqc $OUTPUT
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_TUFA/Reads
-OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_TUFA
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_ITS
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_ITS
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -35,26 +54,54 @@ conda activate multiqc
 
 multiqc $OUTPUT
 
-## WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_ITS
-## OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_ITS
-## 
-## # Make the directory (mkdir) only if not existe already(-p)
-## mkdir -p $OUTPUT
-## 
-## eval "$(conda shell.bash hook)"
-## conda activate fastqc
-## 
-## cd $WORKING_DIRECTORY
-## 
-## for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
-## do
-##       fastqc $FILE -o $OUTPUT
-## done ;
-## 
-## 
-## conda deactivate fastqc
-## conda activate multiqc
-## 
-## # Run multiqc for quality summary
-## 
-## multiqc $OUTPUT
+
+
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_16S
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_16S
+
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
+
+WORKING_DIRECTORY=/scratch_vol1/fungi/Coral_block_colonisation/01_raw_data/Original_reads_18S
+OUTPUT=/scratch_vol1/fungi/Coral_block_colonisation/02_quality_check/Original_reads_18S
+
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
